78c3864bbe92b443194d7386de899d8e987acd4d
buzair6 <buzair6@gmail.com>
2025-07-03 11:05:29 +0500

    goodtogo

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,25 +1,18 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import ErrorBoundary from "./components/ErrorBoundary";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { ThemeProvider } from "./components/ThemeProvider";
-import { TooltipProvider } from "./components/ui/tooltip";
 import { Toaster } from "./components/ui/toaster";
 
 const rootElement = document.getElementById("root")!;
 const root = createRoot(rootElement);
 
+// The root render function no longer uses the ErrorBoundary component,
+// as it was removed to simplify the application.
 root.render(
-  <ErrorBoundary>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <TooltipProvider>
-          <Toaster />
-          <App />
-        </TooltipProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
-  </ErrorBoundary>
+  <QueryClientProvider client={queryClient}>
+    <Toaster />
+    <App />
+  </QueryClientProvider>
 );
